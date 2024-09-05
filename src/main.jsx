1a5a5bbed2c64212b736f665a0c034b4cdf3db8e
oleg131 <dkfjvn@gmail.com>
2024-09-04 17:22:38 -0700

    Add A

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import PiwikPro from "@piwikpro/react-piwik-pro";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+PiwikPro.initialize(
+  "ac075102-eefb-4bea-b73c-b7670783c6d6",
+  "https://marvelous-zuccutto-69c422.piwik.pro"
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
